67faa3288df7e90fe2b99e03bfbb393a32413748
Thomas Catt <armaghanahmed@gmail.com>
2022-11-17 15:31:11 +0500

    lol

--- frontend/assets/views/Home.js
+++ frontend/assets/views/Home.js
@@ -85,10 +85,7 @@ export default function Home(props) {
         }}>
             {
                 /*
-                    Create a transparent dummy <Draggable> here and record draging axes from this Draggable.
-                    Then from the axes, manually displace the currentPressedPiece by calculations from
-                    the axes obtained from the dummy Draggable, such that the <Draggable> acts as a controller
-                    stick for the selected piece to move.
+                    read notes.md
                 */
             }
             <ChessPiece id="whitePawn1" name="pawn" side="white" press={onChessPiecePressed} release={onChessPieceReleased} drag={onChessPieceDragged} position={coordinatesPercentageConversion({percentage: piecesLocations.whitePawn1})}/>
---
+++ notes.md
@@ -0,0 +1,5 @@
+## Dragging stuff in `Home.js`:
+- Create a transparent dummy `<Draggable>` here and record draging axes from this `<Draggable>`.
+- Then from the axes, manually displace the currentPressedPiece by calculations from the axes obtained from the dummy `<Draggable>`.
+- This way, the `<Draggable>` acts as a controller
+stick for the selected piece to move.
